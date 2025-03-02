ac39e3879dbab0a70b5b70fc34cb3c010e737d26
Felipe Manoel <164808012+LipeManoel@users.noreply.github.com>
2025-03-02 12:19:17 -0300

    Delete Projeto 09 - Cambio/src/CurrencyConverter.java

--- Projeto 09 - Cambio/src/CurrencyConverter.java
+++
@@ -1,8 +0,0 @@
-public class CurrencyConverter {
-    public static final double IOF = 0.06;
-
-    //Valor em reais com IOF
-    public static double convert(double dollarRate, double amount) {
-        return amount * dollarRate * (1 + IOF);
-    }
-}
